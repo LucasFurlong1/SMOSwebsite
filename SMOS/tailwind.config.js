32a03a60f3429b3888c5b98574dbf65c0d01f891
Lucas Furlong <trollinc.5@gmail.com>
2022-07-14 16:55:58 -0400

    style changes

--- SMOS/tailwind.config.js
+++ SMOS/tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'bittersweet': '#F87060',
+        'oxford-blue': '#102542',
+        'light-gray': '#CDD7D6',
+        'grullo': '#B3A394',
+        'azure': '#3185FC',
+        'beau-blue': '#C9DBEE',
+        'purpureus': '',
+      }
+    },
+    
   },
   plugins: [],
 }
